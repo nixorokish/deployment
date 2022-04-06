04ff44a57e8c9eafd68d748b6baf65d657adeb3c
kayleen <8iscogb9@anonaddy.me>
2022-04-06 15:23:49 -0500

    html file

--- public/main.js
+++ public/main.js
@@ -1,2 +1,11 @@
 console.log('JS connected')
 
+const addAddressBtn = document.querySelector('#newAddressBtn')
+
+function addInputBoxes() {
+    const addrInput = document.createElement('input');
+    addrInput.type = 'text';
+    addrInput.name = 'test';
+}
+
+addAddressBtn.addEventListener('click', addInputBoxes)
